273783a73a7a5b02c4dab4df17c37e0112a3d64f
Jesper Skov <jskov@mada.dk>
2025-04-15 21:04:10 +0200

    Package bundles with component hierarchy read from POM files
    Also asserts that checksum files are provided for the artifacts.
    
    This is how Publish Portal expects the bundles to look.

--- src/main/java/dk/mada/action/BundleCollector.java
+++ src/main/java/dk/mada/action/BundleCollector.java
@@ -1,10 +1,12 @@
 package dk.mada.action;
 
+import dk.mada.action.util.XmlExtractor;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.jar.JarEntry;
@@ -51,13 +53,14 @@ public final class BundleCollector {
     public List<BundleSource> findBundleSources(Path searchDir, List<String> companionSuffixes) {
         try (Stream<Path> files = Files.walk(searchDir)) {
             // First find the POMs
-            List<Path> poms = files.filter(Files::isRegularFile)
+            List<Pom> poms = files.filter(Files::isRegularFile)
                     .filter(p -> p.getFileName().toString().endsWith(".pom"))
+                    .map(this::readPomMetadata)
                     .toList();
 
             // Then make bundles with the companions
             return poms.stream()
-                    .map(p -> makeSourceBundle(p, companionSuffixes))
+                    .map(pom -> makeSourceBundle(pom, companionSuffixes))
                     .toList();
         } catch (IOException e) {
             throw new IllegalStateException("Failed to build bundles in " + searchDir, e);
@@ -69,18 +72,19 @@ public final class BundleCollector {
      *
      * Finds companion files for the bundle based on the provided suffixes.
      *
-     * @param pomFile           the main POM file
+     * @param pom               the main POM file
      * @param companionSuffixes the suffixes to find companion assets from
      * @return the resulting bundle source
      */
-    private BundleSource makeSourceBundle(Path pomFile, List<String> companionSuffixes) {
+    private BundleSource makeSourceBundle(Pom pom, List<String> companionSuffixes) {
+        Path pomFile = pom.pomFile();
         Path dir = pomFile.getParent();
         String basename = pomFile.getFileName().toString().replace(".pom", "");
         List<Path> companions = companionSuffixes.stream()
                 .map(suffix -> dir.resolve(basename + suffix))
                 .filter(Files::isRegularFile)
                 .toList();
-        return new BundleSource(pomFile, companions);
+        return new BundleSource(pom, companions);
     }
 
     /**
@@ -90,7 +94,7 @@ public final class BundleCollector {
      * @return all files to be included in the bundle (source + signatures)
      */
     private BundleFiles signBundleFiles(BundleSource bundleSrc) {
-        List<Path> signatures = Stream.concat(Stream.of(bundleSrc.pom()), bundleSrc.assets().stream())
+        List<Path> signatures = Stream.concat(Stream.of(bundleSrc.pom().pomFile()), bundleSrc.assets().stream())
                 .map(signer::sign)
                 .toList();
         return new BundleFiles(bundleSrc, signatures);
@@ -103,19 +107,28 @@ public final class BundleCollector {
      * @return the completed bundle
      */
     private Bundle packageBundle(BundleFiles bundleFiles) {
-        Path pom = bundleFiles.bundleSource.pom();
-        Path bundleJar = pom.getParent().resolve(pom.getFileName().toString().replace(".pom", "_bundle.jar"));
+        Pom pom = bundleFiles.bundleSource.pom();
+        Path pomFile = pom.pomFile();
+        Path bundleJar =
+                pomFile.getParent().resolve(pomFile.getFileName().toString().replace(".pom", "_bundle.jar"));
 
         List<Path> allBundleFiles = new ArrayList<>();
-        allBundleFiles.add(pom);
+        allBundleFiles.add(pomFile);
         allBundleFiles.addAll(bundleFiles.bundleSource().assets());
+
+        List<Path> checksumFiles = assertPresenceOfAssociatedChecksumFiles(allBundleFiles);
+
         allBundleFiles.addAll(bundleFiles.signatures());
+        allBundleFiles.addAll(checksumFiles);
+
+        Path jarDirPath = Paths.get(pom.group().replace('.', '/'), pom.artifact(), pom.version());
 
         try (OutputStream os = Files.newOutputStream(bundleJar);
                 BufferedOutputStream bos = new BufferedOutputStream(os);
                 JarOutputStream jos = new JarOutputStream(bos)) {
             for (Path f : allBundleFiles) {
-                JarEntry entry = new JarEntry(f.getFileName().toString());
+                String jarPath = jarDirPath.resolve(f.getFileName()).toString();
+                JarEntry entry = new JarEntry(jarPath);
                 jos.putNextEntry(entry);
                 Files.copy(f, jos);
                 jos.closeEntry();
@@ -126,6 +139,60 @@ public final class BundleCollector {
 
         return new Bundle(bundleJar, bundleFiles);
     }
+
+    /**
+     * Make a list of expected checksum files for the assets, and assert that they
+     * are present.
+     *
+     * @param assets the list of assets
+     * @return the list of asset checksum files
+     */
+    private List<Path> assertPresenceOfAssociatedChecksumFiles(List<Path> assets) {
+        List<Path> checksumFiles = new ArrayList<>();
+        for (Path f : assets) {
+            String name = f.getFileName().toString();
+            checksumFiles.add(f.getParent().resolve(name + ".md5"));
+            checksumFiles.add(f.getParent().resolve(name + ".sha1"));
+        }
+
+        List<Path> missingChecksumFiles =
+                checksumFiles.stream().filter(f -> !Files.isRegularFile(f)).toList();
+        if (!missingChecksumFiles.isEmpty()) {
+            throw new IllegalStateException("Did not find required checksum files: " + missingChecksumFiles);
+        }
+
+        return checksumFiles;
+    }
+
+    /**
+     * Read metadata from the POM file.
+     *
+     * @param pomFile the pom file to read
+     * @return the read POM data
+     */
+    private Pom readPomMetadata(Path pomFile) {
+        try {
+            String pomXml = Files.readString(pomFile);
+            XmlExtractor xex = new XmlExtractor(pomXml);
+            String group = xex.get("groupId");
+            String artifact = xex.get("artifactId");
+            String version = xex.get("version");
+
+            return new Pom(pomFile, group, artifact, version);
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to read POM data from " + pomFile, e);
+        }
+    }
+
+    /**
+     * Information about a POM.
+     *
+     * @param pomFile  the POM file
+     * @param group    the artifact group
+     * @param artifact the artifact name
+     * @param version    the artifact version
+     */
+    public record Pom(Path pomFile, String group, String artifact, String version) {}
 
     /**
      * The packaged bundle.
@@ -146,8 +213,8 @@ public final class BundleCollector {
     /**
      * The original source files of a bundle.
      *
-     * @param pom    the main POM file
+     * @param pom    the main POM
      * @param assets a list of additional assets (may be empty)
      */
-    public record BundleSource(Path pom, List<Path> assets) {}
+    public record BundleSource(Pom pom, List<Path> assets) {}
 }
--- src/test/java/dk/mada/unit/BundleCollectorTest.java
+++ src/test/java/dk/mada/unit/BundleCollectorTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import dk.mada.action.BundleCollector;
 import dk.mada.action.BundleCollector.Bundle;
 import dk.mada.action.BundleCollector.BundleSource;
+import dk.mada.fixture.BundlePrepperFixture;
 import dk.mada.fixture.TestInstances;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.jar.JarFile;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.CleanupMode;
 import org.junit.jupiter.api.io.TempDir;
 
 /**
@@ -21,7 +23,7 @@ import org.junit.jupiter.api.io.TempDir;
  */
 class BundleCollectorTest {
     /** Temporary test directory. */
-    private @TempDir Path testDir;
+    private @TempDir(cleanup = CleanupMode.ALWAYS) Path testDir;
 
     /** The subject under test - the collector. */
     private final BundleCollector sut = TestInstances.bundleCollector();
@@ -31,8 +33,10 @@ class BundleCollectorTest {
      */
     @Test
     void canSignFiles() throws IOException {
+        BundlePrepperFixture.addTestPomFiles(testDir, "bundle.pom");
         Files.copy(Paths.get("gradle/wrapper/gradle-wrapper.jar"), testDir.resolve("bundle.jar"));
-        Files.createFile(testDir.resolve("bundle.pom"));
+        Files.createFile(testDir.resolve("bundle.jar.sha1"));
+        Files.createFile(testDir.resolve("bundle.jar.md5"));
 
         List<Bundle> bundles = sut.collectBundles(testDir, List.of(".jar"));
 
@@ -44,8 +48,50 @@ class BundleCollectorTest {
             assertThat(bundle.files().signatures())
                     .allSatisfy(p -> assertThat(p).isNotEmptyFile());
             assertThat(filesIn(bundle.bundleJar()))
-                    .containsExactlyInAnyOrder("bundle.pom", "bundle.jar", "bundle.pom.asc", "bundle.jar.asc");
+                    .containsExactlyInAnyOrder(
+                            "dk/mada/action-maven-publish-test/0.0.0/bundle.pom",
+                            "dk/mada/action-maven-publish-test/0.0.0/bundle.pom.asc",
+                            "dk/mada/action-maven-publish-test/0.0.0/bundle.pom.sha1",
+                            "dk/mada/action-maven-publish-test/0.0.0/bundle.pom.md5",
+                            "dk/mada/action-maven-publish-test/0.0.0/bundle.jar",
+                            "dk/mada/action-maven-publish-test/0.0.0/bundle.jar.asc",
+                            "dk/mada/action-maven-publish-test/0.0.0/bundle.jar.sha1",
+                            "dk/mada/action-maven-publish-test/0.0.0/bundle.jar.md5");
         });
+    }
+
+    /**
+     * Tests that bundle asset filtering works.
+     */
+    @Test
+    void canCollectRelevantBundleAssets() throws IOException {
+        setupFileTree(
+                "root.jar", // ignored
+                "dir/a.jar", // ignored
+                "dir/a-sources.jar",
+                "dir/a.module");
+        BundlePrepperFixture.addTestPomFiles(testDir.resolve("dir"), "a.pom");
+
+        List<BundleSource> foundBundles =
+                new BundleCollector(null).findBundleSources(testDir, List.of(".module", "-sources.jar"));
+        List<String> foundPaths =
+                foundBundles.stream().flatMap(b -> toPaths(b).stream()).toList();
+        assertThat(foundPaths).containsExactlyInAnyOrder("dir/a.pom", "dir/a-sources.jar", "dir/a.module");
+    }
+
+    private List<String> toPaths(BundleSource bs) {
+        List<Path> files = new ArrayList<>();
+        files.add(bs.pom().pomFile());
+        files.addAll(bs.assets());
+        return files.stream().map(p -> testDir.relativize(p).toString()).toList();
+    }
+
+    private void setupFileTree(String... files) throws IOException {
+        for (String path : files) {
+            Path file = testDir.resolve(path);
+            Files.createDirectories(file.getParent());
+            Files.createFile(file);
+        }
     }
 
     /**
@@ -57,40 +103,6 @@ class BundleCollectorTest {
     private List<String> filesIn(Path jar) throws IOException {
         try (JarFile jf = new JarFile(jar.toFile())) {
             return jf.stream().map(je -> je.getName()).toList();
-        }
-    }
-
-    /**
-     * Tests that bundle asset filtering works.
-     */
-    @Test
-    void canCollectRelevantBundleAssets() throws IOException {
-        setupFileTree(
-                "root.jar", // ignored
-                "dir/a.pom",
-                "dir/a.jar", // ignored
-                "dir/a-sources.jar",
-                "dir/a.module");
-
-        List<BundleSource> foundBundles =
-                new BundleCollector(null).findBundleSources(testDir, List.of(".module", "-sources.jar"));
-        List<String> foundPaths =
-                foundBundles.stream().flatMap(b -> toPaths(b).stream()).toList();
-        assertThat(foundPaths).containsExactlyInAnyOrder("dir/a.pom", "dir/a-sources.jar", "dir/a.module");
-    }
-
-    private List<String> toPaths(BundleSource bs) {
-        List<Path> files = new ArrayList<>();
-        files.add(bs.pom());
-        files.addAll(bs.assets());
-        return files.stream().map(p -> testDir.relativize(p).toString()).toList();
-    }
-
-    private void setupFileTree(String... files) throws IOException {
-        for (String path : files) {
-            Path file = testDir.resolve(path);
-            Files.createDirectories(file.getParent());
-            Files.createFile(file);
         }
     }
 }
